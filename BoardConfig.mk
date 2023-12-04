3c1fbcd6bd8481383d7003d7fad776fde86dd8e3
NopeNopeGuy <kingdutchisbad@gmail.com>
2023-12-04 17:52:02 +0530

    Add Metadata partition

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,11 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/whyred.config
+TARGET_KERNEL_CONFIG += vendor/tulip-perf_defconfig
+
+# Creates metadata partition mount point under root for
+# the devices with metadata parition
+BOARD_USES_METADATA_PARTITION := true
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
